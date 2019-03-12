8c40fdc7151e458dd355ad1ef05cfc83b15140e9
shwetarewatkar <shweta.rewatkar2@gmail.com>
2019-03-11 20:08:33 -0700

    doctor and appointment api fix

--- models/appointment.js
+++ models/appointment.js
@@ -4,7 +4,8 @@ var appointmentInfo = new mongoose.Schema({
     appointmentNumber : {type: String, required:' appointment number is required', unique:true},
     patientID: {type: String, required: 'patient id is required'},
     doctorID: {type: String, required: 'doctor id is required'},
-    appointmentTime : {type: Number, required:'Appointment Time is required'},
+    appointmentDate: {type: String, required: 'appointment date is required'},
+    appointmentTime : {type: String, required:'Appointment Time is required'},
     updated_at : {type: Date, default: Date.now}
 });
 
--- models/doctors.js
+++ models/doctors.js
@@ -1,18 +1,10 @@
 var mongoose = require('mongoose');
 
 var doctorInfo = new mongoose.Schema({
-    doctorId : {type: String, required:' Doctor ID is required', unique:true},
-    doctortName : {type: String, required:'Doctor Name is required'},
-    doctorHours:[
-        {Monday: String, timeStart: String, endTime: String},
-        {Tuesday: String, timeStart: String, endTime: String},
-        {Wednesday: String, timeStart: String, endTime: String},
-        {Thursday: String, timeStart: String, endTime: String},
-        {Friday: String, timeStart: String, endTime: String},
-        {Saturday: String, timeStart: String, endTime: String},
-        {Sunday: String, timeStart: String, endTime: String}
-    ],
-    updated_at : {type: Date, default: Date.now}
+    doctorId: { type: 'String', required:' Doctor ID is required', unique:true },
+	doctortName: { type: 'String', required:' Doctor Name is required' },
+	doctorHours: { type: [ 'Mixed'] },
+	updated_at: { type: 'Date' }
 });
 
 module.exports = mongoose.model('doctorInfo', doctorInfo);
